402fb48cb93163c3e0ce277db96c9ddc3167a83a
Marko <markoalten@web.de>
2020-12-05 14:23:51 +0100

    added more test cases - not tested

--- test/product.js
+++ test/product.js
@@ -90,22 +90,97 @@ contract("product", (accounts) => {
     // check if totalOpenBalance is 0
     assert.equal(await instance.totalOpenBalance({from: hans}), 0);
   })
-  // todo remove
-  /*it("test block advancing", async () => {
-      // get first block number
-      var first = await instance.getBlock.call();
-      // advance 11 blocks
-      for (var i = 0; i < 11; i++)
-      {
-          await instance.checkPayments({from: owner});
-          //await time.advanceBlock();
-          //console.log(await instance.getBlock());
-      }
-      // get second block number
-      var second = await instance.getBlock();
-      //console.log(first);
-      //console.log(second);
-      //console.log("test");
-      assert.equal(first, second - 11);
-  });*/
+
+  it("order two products and pay", async () => {
+    // order product
+    await instance.order(10,  { from: paul});
+    // advance 2 blocks
+    for (var i = 0; i < 2; i++)
+    {
+        await instance.checkPayments({from: owner});
+    }
+    // order second product
+    await instance.order(8,  { from: paul});
+    // check payments
+    await instance.checkPayments({from: owner});
+
+    // calculate the price for both products
+    var price = await instance.price() * 18;
+    // pay the products
+    await instance.pay.sendTransaction({from: paul, value: price});
+    // check if the total open amount is 0
+    assert.equal(await instance.totalOpenBalance({from: eve}), 0);
+  })
+
+  it("order two products, check balance and pay one", async () => {
+    // order product
+    await instance.order(3,  { from: jakob});
+    // check payments
+    await instance.checkPayments({from: owner});
+
+    // order second product
+    await instance.order(11,  { from: jakob});
+    // check payments
+    await instance.checkPayments({from: owner});
+
+    // calculate the price for only one product
+    var price = await instance.price() * 3;
+    // pay only one product
+    await instance.pay.sendTransaction({from: paul, value: price});
+    // check if the total open amount is 0
+    assert.equal(await instance.totalOpenBalance({from: eve}), 0);
+    // check if the total open amount is 11
+    assert.equal(await instance.totalOpenBalance({from: eve}), 11);
+  })
+
+  it("two different people order product and two pay", async () => {
+    // order product with first account
+    await instance.order(8,  { from: phil});
+    // check payments
+    await instance.checkPayments({from: owner});
+
+    // order product with second account
+    await instance.order(1,  { from: josh});
+    // check payments
+    await instance.checkPayments({from: owner});
+
+    // calculate the price for both products individually
+    var priceAccount1 = await instance.price() * 8;
+    var priceAccount2 = await instance.price() * 18;
+    // pay both products
+    await instance.pay.sendTransaction({from: phil, value: priceAccount1});
+    await instance.pay.sendTransaction({from: josh, value: priceAccount2});
+    // check if the total open amount for both accounts 
+    assert.equal(await instance.totalOpenBalance({from: phil}), 0);
+    assert.equal(await instance.totalOpenBalance({from: josh}), 0);
+  })
+
+  it("two different people and only one pays in time", async () => {
+    // order product with first account
+    await instance.order(2,  { from: jay});
+    // advance 10 blocks
+    for (var i = 0; i < 10; i++)
+    {
+        await instance.checkPayments({from: owner});
+    }
+
+    // order product with second account
+    await instance.order(4,  { from: allesha});
+    // check payments
+    await instance.checkPayments({from: owner});
+
+    // calculate the price for on product
+    var price = await instance.price() * 4;
+    // pay both products
+    await instance.pay.sendTransaction({from: allesha, value: price});
+
+    // advance 10 blocks
+    for (var i = 0; i < 10; i++)
+    {
+        await instance.checkPayments({from: owner});
+    }
+
+    // check if the total open amount for both accounts 
+    assert.equal(await instance.totalOpenBalance({from: jay}), 0);
+  })
 });
